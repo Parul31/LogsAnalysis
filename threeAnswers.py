534bcba2c56b3577b9f366c435fddd8c021e3a23
Parul Gupta <parul.uit@gmail.com>
2017-08-23 23:44:46 +0530

    final commit

--- threeAnswers.py
+++ threeAnswers.py
@@ -14,20 +14,20 @@ def getResult(query):
 #question1 is What are the most popular three articles of all time?
 query_for_ques1 = "select title,count(path) as views from articles,log where articles.slug=substring(log.path,10) group by articles.title order by views desc limit 3"
 results_for_ques1 = getResult(query_for_ques1)
-print("What are the most popular three articles of all time?")
+print("\nWhat are the most popular three articles of all time?")
 for result in results_for_ques1:
     print("{:^10} --{:^10}views".format(result[0],result[1]))
 
 #question2 is Who are the most popular article authors of all time?
 query_for_ques2 = "select name,view from authors,ques2 where authors.id=ques2.author order by view desc"
 results_for_ques2 = getResult(query_for_ques2)
-print("Who are the most popular article authors of all time?")
+print("\nWho are the most popular article authors of all time?")
 for result in results_for_ques2 :
     print("{:^10} --{:^10}views".format(result[0],result[1]))
 
 #question3 is On which days did more than 1% of requests lead to errors? 
 query_for_ques3 = "select t1.day,t1.percentage from (select day,round((status_404*100)/status_all::decimal,1) as percentage from ques3) t1 where t1.percentage>1"
 results_for_ques3 = getResult(query_for_ques3)
-print("On which days did more than 1% of requests lead to errors?")
+print("\nOn which days did more than 1% of requests lead to errors?")
 for result in results_for_ques3:
     print("{:^10} --{:^10}%".format(result[0],result[1]))
