f71629a02a5e9c1105da03e3fcfec0af0b1c49ae
Parul Gupta <parul.uit@gmail.com>
2017-08-24 21:08:35 +0530

    pep8 warnings removed

--- threeAnswers.py
+++ threeAnswers.py
@@ -17,9 +17,11 @@ def get_result(query):
 
 # question1 is What are the most popular three articles of all time?
 def get_top_articles():
-    query_for_ques1 = ("select title,count(path) as views "
-    "from articles,log where articles.slug=substring(log.path,10) "
-    "group by articles.title order by views desc limit 3")
+    query_for_ques1 = (
+        "select title,count(path) as views "
+        "from articles,log where articles.slug=substring(log.path,10) "
+        "group by articles.title order by views desc limit 3"
+        )
     results_for_ques1 = get_result(query_for_ques1)
     print("\nWhat are the most popular three articles of all time?")
     for result in results_for_ques1:
@@ -28,8 +30,10 @@ def get_top_articles():
 
 # question2 is Who are the most popular article authors of all time?
 def get_top_authors():
-    query_for_ques2 = ("select name,view from authors,ques2 "
-    "where authors.id=ques2.author order by view desc")
+    query_for_ques2 = (
+        "select name,view from authors,ques2 "
+        "where authors.id=ques2.author order by view desc"
+        )
     results_for_ques2 = get_result(query_for_ques2)
     print("\nWho are the most popular article authors of all time?")
     for result in results_for_ques2:
@@ -38,9 +42,11 @@ def get_top_authors():
 
 # question3 is On which days did more than 1% of requests lead to errors?
 def get_error_percentage():
-    query_for_ques3 = ("select t1.day,t1.percentage "
-    "from (select day,round((status_404*100)/status_all::decimal,1) "
-    "as percentage from ques3) t1 where t1.percentage>1")
+    query_for_ques3 = (
+        "select t1.day,t1.percentage "
+        "from (select day,round((status_404*100)/status_all::decimal,1) "
+        "as percentage from ques3) t1 where t1.percentage>1"
+        )
     results_for_ques3 = get_result(query_for_ques3)
     print("\nOn which days did more than 1% of requests lead to errors?")
     for result in results_for_ques3:
